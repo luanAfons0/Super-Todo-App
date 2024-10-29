2507346369e1132b2fd8ef72e787538d80573a3c
luanAfons0 <luan.hmatao@gmail.com>
2024-10-28 22:32:58 -0300

    feat: created first utils/email validation

---
+++ front-end/utils/validations.ts
@@ -0,0 +1,11 @@
+export const basicEmailValidation = (email: string) => {
+  const isValid = email
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    );
+
+  if (!isValid) {
+    throw new Error("The inserted email is invalid.");
+  }
+};
