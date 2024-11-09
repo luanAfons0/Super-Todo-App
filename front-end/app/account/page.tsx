dd5452a984ef3c0b99b049948301de2989ed739b
luanAfons0 <luan.hmatao@gmail.com>
2024-11-08 21:00:27 -0300

    feat: basic account page

--- front-end/app/account/page.tsx
+++ front-end/app/account/page.tsx
@@ -2,9 +2,9 @@ import UserInfos from "@/components/UserInfos";
 import styles from "./styles.module.scss";
 
 export default function AccountPage() {
-    return (
-        <div className={styles.container}>
-            <UserInfos />
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <UserInfos />
+    </div>
+  );
 }
--- front-end/components/UserInfos/index.tsx
+++ front-end/components/UserInfos/index.tsx
@@ -1,37 +1,92 @@
-"use client"
+"use client";
 
+import { getFromLocalStorage } from "@/utils/localStorage";
+import { basicEmailValidation, validatePassword } from "@/utils/validations";
+import LoadingSpinner from "../LoadingSpinner";
+import { useEffect, useState } from "react";
 import useServer from "@/hook/userServer";
 import styles from "./styles.module.scss";
-import LoadingSpinner from "../LoadingSpinner";
-import { useEffect } from "react";
-import { getFromLocalStorage } from "@/utils/localStorage";
+import { toast } from "react-toastify";
+import Button from "../Button";
+import Input from "../Input";
 
 export default function UserInfos() {
-    const { fetchServer, loading } = useServer()
+    const account = getFromLocalStorage({ key: "account" });
 
-    const successAction = () => {
-        // Set user infos -- TODO
-    }
+    const [firstName, setFirstName] = useState(account?.first_name);
+    const [lastName, setLastName] = useState(account?.last_name);
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [email, setEmail] = useState(account?.email);
+    const [password, setPassword] = useState("");
+    const { fetchServer, loading } = useServer();
 
-    const getUserInfos = async () => {
-        const localStorageAccount = getFromLocalStorage({ key: "account" })
+    const validateUser = async () => {
+        const localStorageAccount = getFromLocalStorage({ key: "account" });
 
         await fetchServer({
             method: "GET",
             route: `account/${localStorageAccount?.id}`,
             headers: { Authorization: `Bearer ${localStorageAccount?.token}` },
-            successAction
-        })
-    }
+        });
+    };
 
     useEffect(() => {
-        getUserInfos()
-    }, [])
+        validateUser();
+    }, []);
+
+    const editFunction = () => {
+        try {
+            validatePassword({ password: password, newPassword: confirmPassword });
+            basicEmailValidation(email);
+        } catch (error) {
+            toast.error((error as Error).message)
+        }
+    }
 
     return (
         <div className={styles.container}>
             {loading && <LoadingSpinner />}
-            <h1>User infos here...</h1>
+            <div className={styles.box}>
+                <div className={styles.row}>
+                    <div className={styles.item}>
+                        <label>First Name:</label>
+                        <Input setValue={setFirstName} type="text" value={firstName} />
+                    </div>
+                    <div className={styles.item}>
+                        <label>Last Name:</label>
+                        <Input setValue={setLastName} type="text" value={lastName} />
+                    </div>
+                    <div className={styles.item}>
+                        <label>Email:</label>
+                        <Input setValue={setEmail} type="text" value={email} />
+                    </div>
+                    <div className={styles.item}>
+                        <label>Password:</label>
+                        <Input setValue={setPassword} type="password" value={password} />
+                    </div>
+                    <div className={styles.item}>
+                        <label>Confirm password:</label>
+                        <Input
+                            setValue={setConfirmPassword}
+                            type="password"
+                            value={confirmPassword}
+                        />
+                    </div>
+                </div>
+                <hr />
+                <div className={styles.row}>
+                    <Button
+                        buttonText="Edit"
+                        customStyle={{
+                            padding: "0.5rem 1rem",
+                            margin: "0",
+                            alignSelf: "flex-end",
+                            marginLeft: "auto",
+                        }}
+                        onClick={editFunction}
+                    />
+                </div>
+            </div>
         </div>
-    )
+    );
 }
