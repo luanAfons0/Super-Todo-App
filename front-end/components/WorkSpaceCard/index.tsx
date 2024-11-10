a12a65319ebcd345b1513b25d415420a84537c51
luanAfons0 <luan.hmatao@gmail.com>
2024-11-10 19:55:30 -0300

    feat: finished read and create workspace in front-end

--- front-end/components/WorkSpaceCard/index.tsx
+++ front-end/components/WorkSpaceCard/index.tsx
@@ -1,29 +1,44 @@
+import { PencilRuler, Trash2 } from "lucide-react";
 import styles from "./styles.module.scss";
 import { CirclePlus } from "lucide-react";
 import ToolTip from "../ToolTip";
 
 export type TypeWorkSpaceCard = {
   description: string;
+  onClick: Function;
   name: string;
   id: number;
-  onClick: Function;
 };
 
 export default function WorkSpaceCard({
   description,
-  id,
-  name,
   onClick,
+  name,
+  id,
 }: TypeWorkSpaceCard) {
   return (
     <ToolTip tootTipText={description}>
       {id === 0 ? (
-        <div className={styles.container} onClick={() => onClick()}>
+        <div
+          style={{ justifyContent: "center" }}
+          className={styles.container}
+          onClick={() => onClick()}
+        >
           <CirclePlus size={100} />
         </div>
       ) : (
-        <div className={styles.container} onClick={() => onClick()}>
-          <h3>{name}</h3>
+        <div className={`${styles.container}`}>
+          <div className={styles.cardHeader} onClick={() => onClick()}>
+            <h3>{name}</h3>
+          </div>
+          <div className={styles.cardFooter}>
+            <button>
+              <PencilRuler size={20} />
+            </button>
+            <button>
+              <Trash2 size={20} />
+            </button>
+          </div>
         </div>
       )}
     </ToolTip>
